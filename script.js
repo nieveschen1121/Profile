a3eaca02a1373e8f6dda8dfe946f01b51bcb6f7d
Neives <neiveschen1121@gmail.com>
2019-09-08 00:03:49 +0800

    Add page v1.

---
+++ script.js
@@ -0,0 +1,145 @@
+var resume = {
+  profile: {
+    name: "陳亭秀",
+    application: "課程行銷",
+    introduction: "Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，Hi，我是亭秀，...",
+    mobile: "+886912345678",
+    email: "testtesttest@gmail.com"
+  },
+  events: [
+    {
+      organization: "萬泰水塔高雄廠",
+      position: "創廠人、經理", 
+      duration: {
+        beginYYYY: 2018, 
+        beginMM: 7,
+        endYYYY: 2019, 
+        endMM: 6
+      }, 
+      description: "工廠選址、廠商簽約、人員訓練、財務管理、業務拜訪、客訴處理、數據分析、廣告投放、粉專經營", 
+      items: [
+        { description: "僅用 8 萬的起始成本創廠，營業兩月後打平收支開始獲利" }, 
+        { description: "獨立洽談 320 萬的專案訂單成功" },
+        { description: "提升公司總營業額 25%" },
+        { description: "培育 4 位新人，穩定公司制度" },
+      ]
+    },
+    {
+      organization: "萬泰水塔高雄廠",
+      position: "創廠人、經理", 
+      duration: {
+        beginYYYY: 2018, 
+        beginMM: 7,
+        endYYYY: 2019, 
+        endMM: 6
+      }, 
+      description: "工廠選址、廠商簽約、人員訓練、財務管理、業務拜訪、客訴處理、數據分析、廣告投放、粉專經營", 
+      items: [
+        { description: "僅用 8 萬的起始成本創廠，營業兩月後打平收支開始獲利" }, 
+        { description: "獨立洽談 320 萬的專案訂單成功" },
+        { description: "提升公司總營業額 25%" },
+        { description: "培育 4 位新人，穩定公司制度" },
+      ]
+    },
+    {
+      organization: "萬泰水塔高雄廠",
+      position: "創廠人、經理", 
+      duration: {
+        beginYYYY: 2018, 
+        beginMM: 7,
+        endYYYY: 2019, 
+        endMM: 6
+      }, 
+      description: "工廠選址、廠商簽約、人員訓練、財務管理、業務拜訪、客訴處理、數據分析、廣告投放、粉專經營", 
+      items: [
+        { description: "僅用 8 萬的起始成本創廠，營業兩月後打平收支開始獲利" }, 
+        { description: "獨立洽談 320 萬的專案訂單成功" },
+        { description: "提升公司總營業額 25%" },
+        { description: "培育 4 位新人，穩定公司制度" },
+      ]
+    },
+    {
+      organization: "萬泰水塔高雄廠",
+      position: "創廠人、經理", 
+      duration: {
+        beginYYYY: 2018, 
+        beginMM: 7,
+        endYYYY: 2019, 
+        endMM: 6
+      }, 
+      description: "工廠選址、廠商簽約、人員訓練、財務管理、業務拜訪、客訴處理、數據分析、廣告投放、粉專經營", 
+      items: [
+        { description: "僅用 8 萬的起始成本創廠，營業兩月後打平收支開始獲利" }, 
+        { description: "獨立洽談 320 萬的專案訂單成功" },
+        { description: "提升公司總營業額 25%" },
+        { description: "培育 4 位新人，穩定公司制度" },
+      ]
+    },
+    {
+      organization: "萬泰水塔高雄廠",
+      position: "創廠人、經理", 
+      duration: {
+        beginYYYY: 2018, 
+        beginMM: 7,
+        endYYYY: 2019, 
+        endMM: 6
+      }, 
+      description: "工廠選址、廠商簽約、人員訓練、財務管理、業務拜訪、客訴處理、數據分析、廣告投放、粉專經營", 
+      items: [
+        { description: "僅用 8 萬的起始成本創廠，營業兩月後打平收支開始獲利" }, 
+        { description: "獨立洽談 320 萬的專案訂單成功" },
+        { description: "提升公司總營業額 25%" },
+        { description: "培育 4 位新人，穩定公司制度" },
+      ]
+    },
+  ], 
+  skills: {
+    language: [
+      {
+        name: "中文",
+        level: 3
+      },
+      {
+        name: "台語",
+        level: 3
+      },
+      {
+        name: "英文",
+        level: 2
+      },
+      {
+        name: "西班牙文",
+        level: 2
+      },
+      {
+        name: "印尼文",
+        level: 1
+      },
+      {
+        name: "日文",
+        level: 1
+      },
+    ],
+    computer: [
+      { name: "MS Office" },
+      { name: "MS Power BI" },
+      { name: "Adobe Illustrator" },
+      { name: "Adobe Photoshop" },
+    ],
+  },
+  certificates: [
+    { name: "BULATS 國際職場英語檢定 B1" },
+    { name: "BULATS 國際職場西語檢定 B1" },
+    { name: "資策會 SEO 網路行銷曝光實務" },
+  ]
+}
+var levelLookup = {
+  "1": "基礎", 
+  "2": "中等",
+  "3": "流利"
+};
+
+var vm = new Vue({
+  el: "#app", 
+  data: resume
+});
